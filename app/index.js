c1632e1ff83ff3b2cd7573ba87dce42fddbca9f6
Chris Contolini <christopher.contolini@cfpb.gov>
2014-10-08 12:02:23 -0400

    Improve bower installation

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
 var chalk = require('chalk');
 var multiline = require('multiline');
 
@@ -11,25 +12,22 @@ var CapitalFrameworkGenerator = yeoman.generators.Base.extend({
 
     this.on('end', function () {
       if (!this.options['skip-install']) {
-        this.installDependencies();
+        this.npmInstall();
       }
     });
   },
 
-  askFor: function () {
+  promptTask: function () {
+
     var done = this.async();
-    var log = this.log;
 
-    var welcome = this.yeoman + multiline.stripIndent(function () {/*
-      Welcome to Capital Framework's generator!
+    // Have Yeoman greet the user.
+    this.log( yosay("Welcome to Capital Framework's generator!") );
 
-      To learn about Capital Framework, visit https://cfpb.github.io/capital-framework/
-    */});
+    this.log( 'To learn about Capital Framework, visit https://cfpb.github.io/capital-framework/' );
 
-    log( welcome );
-
-    var prompts = [{
-      type: 'list',
+    this.prompt({
+      type: 'checkbox',
       name: 'components',
       message: 'Which CF components would you like in your app?',
       choices: [
@@ -43,34 +41,32 @@ var CapitalFrameworkGenerator = yeoman.generators.Base.extend({
         { name: 'Typography', value: 'typography' },
       ],
       default: true
-    }];
-
-    this.prompt(prompts, function ( props ) {
-      this.slug = this._.slugify( props.type );
+    }, function ( answers ) {
+      // Copy over bower.json before installing stuff.
+      this.bulkCopy('bower.json', 'bower.json');
+      // Install the components. This is probably installing them in the wrong
+      // dir since we actually want to use `grunt vendor`. The nice thing about
+      // bowerInstall is that it's writing the dependencies to bower.json. Maybe
+      // we can copy the components to the correct directory about bowerInstall
+      // completes?
+      answers.components.forEach(function( component ) {
+        this.bowerInstall([ 'cfpb/cf-' + component ], { 'save': true });
+      }.bind(this));
       done();
     }.bind(this));
   },
 
-  jquery: function source() {
+  generic: function source() {
 
-    // src files
-    this.mkdir('src');
-    this.copy('jquery-plugin/src/jshintrc', 'src/.jshintrc');
-    this.template('jquery-plugin/src/name.js', 'src/' + this.slug + '.js');
-
-    // test files
-    this.mkdir('test');
-    this.copy('jquery-plugin/test/jshintrc', 'test/.jshintrc');
-    this.template('jquery-plugin/test/name_test.js', 'test/' + this.slug + '_test.js');
-    this.template('jquery-plugin/test/name.html', 'test/' + this.slug + '.html');
-
-    // general files
-    this.copy('jquery-plugin/jshintrc', '.jshintrc');
-    this.copy('jquery-plugin/gitignore', '.gitignore');
-    this.copy('jquery-plugin/travis.yml', '.travis.yml');
-    this.template('jquery-plugin/Gruntfile.js', 'Gruntfile.js');
-    this.template('jquery-plugin/_bower.json', 'bower.json');
-    this.template('jquery-plugin/_package.json', 'package.json');
+    // This is a really unexhaustive list and the files aren't even correct. For
+    // now they're basically just placeholders.
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('COPYING.txt', 'COPYING.txt');
+    this.copy('gitignore', '.gitignore');
+    this.copy('Gruntfile.js', 'Gruntfile.js');
+    this.copy('package.json', 'package.json');
+    this.copy('README.md', 'README.md');
+    this.copy('TERMS.md', 'TERMS.md');
 
   }
 });
