b0585ab2f00df0ce739c4bf6e2d1c736ad42d962
Chris Contolini <christopher.contolini@cfpb.gov>
2014-10-26 02:39:37 -0400

    All sorts of build flow updates

--- app/index.js
+++ app/index.js
@@ -1,73 +1,137 @@
 'use strict';
-var util = require('util');
-var path = require('path');
+
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
-var chalk = require('chalk');
-var multiline = require('multiline');
+var path = require('path');
+
+// Eventually we'll generate this list dynamically from GitHub.
+var frameworkComponents = [
+  { name: 'Core', value: 'cf-core' },
+  { name: 'Buttons', value: 'cf-buttons' },
+  { name: 'Expandables', value: 'cf-expandables' },
+  { name: 'Forms', value: 'cf-forms' },
+  { name: 'Grid', value: 'cf-grid' },
+  { name: 'Icons', value: 'cf-icons' },
+  { name: 'Pagination', value: 'cf-pagination' }
+];
 
 var CapitalFrameworkGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.npmInstall();
-      }
-    });
+  initializing: {
+
+    greet: function() {
+      this.pkg = require('../package.json');
+      this.log( yosay('Welcome to Capital Framework\'s generator!') );
+      this.log('To learn about Capital Framework, visit http://capitalframework.com');
+    }
+
   },
 
-  promptTask: function () {
+  prompting: {
 
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log( yosay("Welcome to Capital Framework's generator!") );
-
-    this.log( 'To learn about Capital Framework, visit https://cfpb.github.io/capital-framework/' );
-
-    this.prompt({
-      type: 'checkbox',
-      name: 'components',
-      message: 'Which CF components would you like in your app?',
-      choices: [
-        { name: 'Core', value: 'core' },
-        { name: 'Buttons', value: 'buttons' },
-        { name: 'Expandables', value: 'expandables' },
-        { name: 'Forms', value: 'forms' },
-        { name: 'Grid', value: 'grid' },
-        { name: 'Icons', value: 'icons' },
-        { name: 'Pagination', value: 'pagination' },
-      ],
-      default: true
-    }, function ( answers ) {
-      // Copy over bower.json before installing stuff.
-      this.bulkCopy('bower.json', 'bower.json');
-      // Install the components. This is probably installing them in the wrong
-      // dir since we actually want to use `grunt vendor`. The nice thing about
-      // bowerInstall is that it's writing the dependencies to bower.json. Maybe
-      // we can copy the components to the correct directory about bowerInstall
-      // completes?
-      answers.components.forEach(function( component ) {
-        this.bowerInstall([ 'cfpb/cf-' + component ], { 'save': true });
+    askForName: function() {
+      var done = this.async();
+      this.prompt({
+        name: 'name',
+        message: 'What is the name of your project?',
+        default: this._.humanize( path.basename(process.cwd()) ),
+      }, function( answers ) {
+        this.slugname = this._.slugify( answers.name );
+        this.safeSlugname = this.slugname.replace( /-+([a-zA-Z0-9])/g, function ( g ) {
+            return ' ' + g[1].toUpperCase();
+          }
+        );
+        done();
       }.bind(this));
-      done();
-    }.bind(this));
+    },
+
+    askForDescription: function() {
+      var done = this.async();
+      var prompts = [{
+        name: 'description',
+        message: 'Description',
+        default: 'The best website ever.'
+      }, {
+        name: 'homepage',
+        message: 'Project\'s homepage'
+      }, {
+        name: 'license',
+        message: 'Project\'s License',
+        default: 'MIT'
+      }, {
+        name: 'authorName',
+        message: 'Author\'s name'
+      }, {
+        name: 'authorEmail',
+        message: 'Author\'s email'
+      }, {
+        name: 'authorUrl',
+        message: 'Author\'s homepage'
+      }];
+      this.prompt(prompts, function ( answers ) {
+        this.currentYear = (new Date()).getFullYear();
+        this.props = answers;
+        done();
+      }.bind(this));
+    },
+
+    askAboutComponents: function() {
+      var done = this.async();
+      this.prompt({
+        type: 'checkbox',
+        name: 'components',
+        message: 'Which CF components would you like in your app?',
+        choices: frameworkComponents
+      }, function ( answers ) {
+        this.components = answers.components;
+        done();
+      }.bind(this));
+    }
+
   },
 
-  generic: function source() {
+  writing: {
 
-    // This is a really unexhaustive list and the files aren't even correct. For
-    // now they're basically just placeholders.
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('COPYING.txt', 'COPYING.txt');
-    this.copy('gitignore', '.gitignore');
-    this.copy('Gruntfile.js', 'Gruntfile.js');
-    this.copy('package.json', 'package.json');
-    this.copy('README.md', 'README.md');
-    this.copy('TERMS.md', 'TERMS.md');
+    appFiles: function() {
+      this.template('_README.md', 'README.md');
+      this.template('_Gruntfile.js', 'Gruntfile.js');
+      this.template('_package.json', 'package.json');
+      this.template('_bower.json', 'bower.json');
+      this.copy('bowerrc', '.bowerrc');
+      this.copy('COPYING.txt', 'COPYING.txt');
+      this.copy('gitignore', '.gitignore');
+      this.copy('TERMS.md', 'TERMS.md');
+    },
+
+    srcFiles: function() {
+      this.mkdir('src');
+      this.directory('src/static', 'src/static');
+      this.mkdir('dist');
+    },
+
+  },
+
+  install: function() {
+
+    var done = this.async(),
+        actuallyDone = this._.after(2, done),
+        components = this.components.map( function( component ) {
+          return 'cfpb/' + component;
+        });
+    this.npmInstall( '', {}, actuallyDone );
+    this.bowerInstall( components, {'save': true}, actuallyDone );
+
+  },
+
+  end: {
+
+    bye: function(){
+      this.log( yosay('All done! Happy hacking!') );
+    }
 
   }
+
+
 });
 
 module.exports = CapitalFrameworkGenerator;
