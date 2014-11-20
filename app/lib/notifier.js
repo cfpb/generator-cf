aa894448b78499bdbe2bb97171c958d7cc3b1709
Chris Contolini <christopher.contolini@cfpb.gov>
2014-11-20 18:38:54 -0500

    Alert user if a newer version of the generator is available

---
+++ app/lib/notifier.js
@@ -0,0 +1,16 @@
+var updateNotifier = require('update-notifier');
+var pkg = require('../../package.json');
+
+module.exports = function() {
+
+  var notifier = updateNotifier({
+    packageName: pkg.name,
+    packageVersion: pkg.version,
+    updateCheckInterval: 1000 * 60 * 60 * 1 // 1 hr
+  });
+
+  if (notifier.update) {
+    notifier.notify(true);
+  }
+
+};
