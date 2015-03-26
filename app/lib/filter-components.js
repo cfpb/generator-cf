15e94113301df39ec014526df24f0f399ff87ded
jimmynotjim <jimmynotjim@me.com>
2015-03-25 19:05:51 -0500

    Switching GitHub API to search endpoint
    - using search endpoint reduces total number of requests (we now have over 100 repos)
    - using search endpoint greatly reduces request times (removes about 1-2 sec from initial ramp up)
    - using search endpoint allows us to push the filtering on to GitHub rather than our app’s logic

--- app/lib/filter-components.js
+++ app/lib/filter-components.js
@@ -2,26 +2,19 @@ var ignore = require('./ignore.json');
 var frameworkComponents = [];
 
 function getCfComponents( data ) {
-  data.forEach( function(el, i) {
+  data.items.forEach( function(el, i) {
 
     var name;
-    // Does it start with "cf-"?
-    var startsWithCf = el.name.indexOf('cf-') === 0;
     // Is it listed in the ignore.json file?
     var ignoreIt = ignore.indexOf( el.name ) >= 0;
-    // Is the word deprecated in the description?
-    var isDeprecated = el.description.toLowerCase().indexOf('deprecated') >= 0;
-    // Has GitHub determined its language (meaning it's probably not an empty repo.)?
-    var hasLanguage = el.language !== null;
 
-    if ( startsWithCf && !ignoreIt && !isDeprecated && hasLanguage ) {
+    if ( !ignoreIt ) {
       name = el.name.replace( 'cf-', '' );
       frameworkComponents.push({
         name: name.charAt(0).toUpperCase() + name.slice(1),
         value: el.name
       });
     }
-
   });
   // Sort them alphabetically
   return frameworkComponents.sort( function( a, b ) {
